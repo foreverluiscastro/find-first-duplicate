7c9714257159914fa8a872943fc7ee46e39cbd8d
Luis Castro <foreverluiscastro@gmail.com>
2023-09-20 11:05:20 -0400

    Done

--- javascript/find_first_duplicate.js
+++ javascript/find_first_duplicate.js
@@ -1,6 +1,22 @@
 function findFirstDuplicate(arr) {
   // type your code here
+  let refString = ''
+  for (const num of arr) {
+    debugger
+    if (refString.includes(num)) {
+      debugger
+      return num
+    } else {
+      debugger
+      refString = refString.concat(num)
+    }
+  }
+  return -1
 }
+
+findFirstDuplicate([2,1,3,3,2])
+
+// console.log(typeof ("".concat(5)))
 
 if (require.main === module) {
   // add your own tests in here
@@ -16,4 +32,29 @@ if (require.main === module) {
 module.exports = findFirstDuplicate;
 
 // Please add your pseudocode to this file
-// And a written explanation of your solution
+
+/****************** First Idea ***********************
+ *  create a new empty ref string
+ * 
+ *  iterate through the array
+ *    if the ref string includes the element as a substring
+ *      return element
+ *    else take the element and concat it to the empty string
+ * 
+ * if there are no dupes, return -1
+ * 
+ * ***************************************************/
+
+/******************************************************
+ * the function seems to be going in order from left to
+ * right and returning the first duplicate in the sequence.
+ * the empty reference string is to keep track of what numbers
+ * have been iterated through so far. Then I use the string 
+ * includes method to see if the element has already been added
+ * to the reference string. if it is then we have a dupe
+ * and if not, then its the first time the number has
+ * come up in the sequence to mark it down and keep iterating.
+ * then once the loop is done, that means the code never found a 
+ * duplicate so return -1.
+ * 
+ * ***************************************************/
